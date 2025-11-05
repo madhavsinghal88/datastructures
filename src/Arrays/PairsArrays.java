13626658cd42e931074faafd25445e28e1fd1d43
Madhav Singhal <madhav8official@gmail.com>
2025-11-05 15:38:03 +0530

    Pair Arrays

--- src/Arrays/PairsArrays.java
+++ src/Arrays/PairsArrays.java
@@ -2,13 +2,16 @@ package Arrays;
 
 public class PairsArrays {
     public static void ArPair(int[] numb){
+        int totalAr = 0;
         for(int i = 0; i<numb.length; i++){
             int curr = numb[i];
             for(int j = i+1; j<numb.length; j++){
                 System.out.print("(" + curr + "," + numb[j] + ")");
+                totalAr++;
             }
             System.out.println();
         }
+        System.out.println("Total Pair Value : "+totalAr);
     }
     public static void main(String[] args){
         int[] numb = {2,4,6,8,10,12};
@@ -17,3 +20,4 @@ public class PairsArrays {
 
 
 }
+// TOTAL NUMBER OF PAIRS = n(n-1)/2 or sum of n-1
