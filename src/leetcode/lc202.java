62776412394929ee8715503eb477e91f0498c780
Madhav Singhal <madhavsinghal880@gmail.com>
2026-03-22 23:25:49 +0530

    feat: add LeetCode 202 Happy Number solution
    Implements isHappy using Floyd's cycle detection; includes getNext
    helper and a small main for demonstration.

---
+++ src/leetcode/lc202.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class lc202 {
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while (true) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        return slow == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        lc202 obj = new lc202();
+
+        int n = 19;   
+        boolean result = obj.isHappy(n);
+
+        System.out.println("Is " + n + " a Happy Number? " + result);
+    }
+}
